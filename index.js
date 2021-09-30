332775a31f61a845a529c054c28f164efff41349
unknown <zachariah11jerobert@gmail.com>
2021-09-30 08:12:18 +0530

    step2 - create reducers

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
-const BUY_CAKE='BUY_CAKE'
+const BUY_CAKE = "BUY_CAKE";
 
-function buyCake(){
-    return{
-        type:BUY_CAKE,
-        info:'First redux action'
-    }
+function buyCake() {
+  return {
+    type: BUY_CAKE,
+    info: "First redux action",
+  };
 }
+
+const initialState = {
+  numOfCakes: 10,
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case BUY_CAKE:
+      return {
+        ...state,
+        numOfCakes: state.numOfCakes - 1,
+      };
+
+    default:
+      return state;
+  }
+};
